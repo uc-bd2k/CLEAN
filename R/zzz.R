e4a479e84a53454fd9f105851c6732f34ae0fe7e
Mario-Medvedovic <mario.medvedovic@gmail.com>
2017-03-02 07:05:09 -0500

    Fixing treeview path

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,11 @@
 function(lib, pkg)
 {
     #.fTreeViewPath <- paste(.Library, "/CLEAN/doc/", sep = "")
-    .fTreeViewPath <- paste(system.file(package = "CLEAN"), "/doc/", sep = "")
-    assign(".fTreeViewPath", paste(.Library, "/CLEAN/doc/", sep = ""), envir = sys.frame())
+    #    .fTreeViewPath <- paste(system.file(package = "CLEAN"), "/doc/", sep = "")
+    #    assign(".fTreeViewPath", paste(.Library, "/CLEAN/doc/", sep = ""), envir = sys.frame())
+    for(cpath in .libPaths()){
+        cfpath<-paste(cpath, "/CLEAN/doc/", sep="")
+        if(file.exists(paste(cfpath,"LFTreeView.jar", sep="")) | file.exists(paste(cfpath,"TreeView.jar", sep="")))
+            assign(".fTreeViewPath",cfpath,envir=sys.frame())
 }
 
