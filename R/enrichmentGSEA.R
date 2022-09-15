5fc6338e51fdd06bbb29f805a9ab871af4d6c8e4
Mario-Medvedovic <Mario.Medvedovic@uc.edu>
2022-09-15 08:22:50 -0400

    restoring enrichmentGSEA conflicts

--- R/enrichmentGSEA.R
+++ R/enrichmentGSEA.R
@@ -1,10 +1,13 @@
-'enrichmentGSEA'<-
-  function(sigvals,geneids,diffexp=NULL, functionalCategories=c("KEGG","GO","tfacts","dorotheatfs","hallmark"), directions=c("up","down","both"),species="Hs", excel=F,sigFDR=0.1,nperm=100000,minSize=5,maxSize=1500){
-  require(CLEAN);require(CLEAN.Hs);require(fgsea)
+'enrichmentGSEA'<-  function(sigvals,geneids,diffexp=NULL, functionalCategories=c("KEGG","GO","tfacts","dorotheatfs","hallmark"), directions=c("up","down","both"),species="Hs", excel=F,sigFDR=0.1,nperm=100000,minSize = 15, maxSize = 500){
+  require(CLEAN)
+  print(paste0("CLEAN.",species),character.only=T)
+  require(paste0("CLEAN.",species),character.only=T)
+  require(fgsea)
+
   if(is.list(functionalCategories)) gs<-functionalCategories
   else gs<-getFunctionalCategories(functionalCategories,species)
   outlist=as.list(NULL)
-  
+
   if("both" %in% directions){
     if(is.null(diffexp)){
       stats<-sigvals
@@ -16,40 +19,52 @@
       stats=sort(stats)
     }
     fgseaRes <- lapply(gs, function(x){
-      fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm)
+      if(is.null(nperm)) {
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize)
+      } else{
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm)
+      }
       fgseaOut<-data.frame(ID=fgseaOut$pathway,Name=x[[2]][match(fgseaOut$pathway,x[[2]][,1]),2],fgseaOut[,-1],stringsAsFactors = F)
       fgseaOut[order(fgseaOut$pval),]
     })
     names(fgseaRes)<-names(gs)
     outlist=c(outlist,list(both=fgseaRes))
   }
-  
+
   if("up" %in% directions){
     stats=-log10(ifelse(diffexp>0,sigvals/2,1-sigvals/2))
     names(stats)<-geneids
     stats=sort(stats)
     fgseaRes <- lapply(gs, function(x){
-      fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm)
+      if(is.null(nperm)) {
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=15, maxSize=500)
+      } else{
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=15, maxSize=500, nperm=nperm)
+      }
       fgseaOut<-data.frame(ID=fgseaOut$pathway,Name=x[[2]][match(fgseaOut$pathway,x[[2]][,1]),2],fgseaOut[,-1],stringsAsFactors = F)
       fgseaOut[order(fgseaOut$pval),]
     })
     names(fgseaRes)<-names(gs)
     outlist=c(outlist,list(up=fgseaRes))
   }
-  
+
   if("down" %in% directions){
     stats=-log10(ifelse(diffexp<0,sigvals/2,1-sigvals/2))
     names(stats)<-geneids
     stats=sort(stats)
     fgseaRes <- lapply(gs, function(x){
-      fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm)
+      if(is.null(nperm)) {
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=15, maxSize=500)
+      } else{
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=15, maxSize=500, nperm=nperm)
+      }
       fgseaOut<-data.frame(ID=fgseaOut$pathway,Name=x[[2]][match(fgseaOut$pathway,x[[2]][,1]),2],fgseaOut[,-1],stringsAsFactors = F)
       fgseaOut[order(fgseaOut$pval),]
     })
     names(fgseaRes)<-names(gs)
     outlist=c(outlist,list(down=fgseaRes))
   }
-  
+
   if(excel){
     require(xlsx)
     upexcel <- createWorkbook()
@@ -60,7 +75,7 @@
       # addDataFrame(x=get(paste0("rsup$",geneset)),sheet=paste0("up",geneset))
       addDataFrame(x=rsup[geneset][[1]],sheet=get(paste0("up",geneset)))
     }
-    outlist=c(outlist,list(upexcel=upexcel))     
+    outlist=c(outlist,list(upexcel=upexcel))
     downexcel <- createWorkbook()
     for (geneset in genesets){
       assign(paste0("down",geneset),createSheet(wb=downexcel,sheetName=paste0("down",geneset)))
@@ -69,7 +84,7 @@
       # addDataFrame(x=get(paste0("rsup$",geneset)),sheet=paste0("up",geneset))
       addDataFrame(x=rsdown[geneset][[1]],sheet=get(paste0("down",geneset)))
     }
-    outlist=c(outlist,list(downexcel=downexcel))     
+    outlist=c(outlist,list(downexcel=downexcel))
     bothexcel <- createWorkbook()
     for (geneset in genesets){
       assign(paste0("both",geneset),createSheet(wb=bothexcel,sheetName=paste0("both",geneset)))
@@ -78,7 +93,7 @@
       # addDataFrame(x=get(paste0("rsup$",geneset)),sheet=paste0("up",geneset))
       addDataFrame(x=rsboth[geneset][[1]],sheet=get(paste0("both",geneset)))
     }
-    outlist=c(outlist,list(bothexcel=bothexcel))     
+    outlist=c(outlist,list(bothexcel=bothexcel))
   }
   return(outlist)
 }
