33e820dfe4e51817cf0bafe166349b54db073a76
Mario-Medvedovic <Mario.Medvedovic@uc.edu>
2024-10-14 23:03:09 -0400

    enrichmentGSEA fixing combined option

--- R/enrichmentGSEA.R
+++ R/enrichmentGSEA.R
@@ -1,4 +1,4 @@
-'enrichmentGSEA'<-  function(sigvals,geneids,diffexp=NULL, functionalCategories=c("KEGG","GO","tfacts","dorotheatfs","hallmark"), directions=c("up","down","both"),species="Hs", excel=F,sigFDR=0.1,nperm=100000,minSize = 15, maxSize = 500){
+'enrichmentGSEA'<-  function(sigvals=NULL,geneids,diffexp=NULL, functionalCategories=c("KEGG","GO","tfacts","dorotheatfs","hallmark"), directions=c("up","down","both","combined"),species="Hs", excel=F,sigFDR=0.1,nperm=100000,minSize = 15, maxSize = 500){
   require(CLEAN)
   print(paste0("CLEAN.",species),character.only=T)
   require(paste0("CLEAN.",species),character.only=T)
@@ -7,20 +7,25 @@
   if(is.list(functionalCategories)) gs<-functionalCategories
   else gs<-getFunctionalCategories(functionalCategories,species)
   outlist=as.list(NULL)
-
+# print(paste0("is.null(sigvals)=",is.null(sigvals)))
+# print(paste0("is.null(diffexp)=",is.null(diffexp)))
   if("both" %in% directions){
     if(is.null(diffexp)){
       stats<-sigvals
       names(stats)<-geneids
       stats=sort(stats)
-    }else{
+    }else if(is.null(sigvals)){
+      stats<-diffexp
+      names(stats)<-geneids
+      stats<-sort(stats)
+    }else {
       stats<-ifelse(diffexp>0,-log10(sigvals),log10(sigvals))
       names(stats)<-geneids
       stats=sort(stats)
     }
     fgseaRes <- lapply(gs, function(x){
       if(is.null(nperm)) {
-        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize)
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize,eps=0)
       } else{
         fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm)
       }
@@ -37,9 +42,9 @@
     stats=sort(stats)
     fgseaRes <- lapply(gs, function(x){
       if(is.null(nperm)) {
-        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=15, maxSize=500)
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize)
       } else{
-        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=15, maxSize=500, nperm=nperm)
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm)
       }
       fgseaOut<-data.frame(ID=fgseaOut$pathway,Name=x[[2]][match(fgseaOut$pathway,x[[2]][,1]),2],fgseaOut[,-1],stringsAsFactors = F)
       fgseaOut[order(fgseaOut$pval),]
@@ -54,9 +59,9 @@
     stats=sort(stats)
     fgseaRes <- lapply(gs, function(x){
       if(is.null(nperm)) {
-        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=15, maxSize=500)
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize)
       } else{
-        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=15, maxSize=500, nperm=nperm)
+        fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm)
       }
       fgseaOut<-data.frame(ID=fgseaOut$pathway,Name=x[[2]][match(fgseaOut$pathway,x[[2]][,1]),2],fgseaOut[,-1],stringsAsFactors = F)
       fgseaOut[order(fgseaOut$pval),]
@@ -64,6 +69,82 @@
     names(fgseaRes)<-names(gs)
     outlist=c(outlist,list(down=fgseaRes))
   }
+
+  if("combined" %in% directions){
+    statsUp=-log10(ifelse(diffexp>0,sigvals/2,1-sigvals/2))
+    names(statsUp)<-geneids
+    statsUp=sort(statsUp)
+    print(head(statsUp))
+    statsDown=-log10(ifelse(diffexp<0,sigvals/2,1-sigvals/2))
+    names(statsDown)<-geneids
+    statsDown=sort(statsDown)
+    print(head(statsDown))
+    fgseaRes <- lapply(gs, function(x){
+      if(is.null(nperm)) {
+        fgseaOutUp<-fgsea(pathways = x[[1]],stats = statsUp, minSize=minSize, maxSize=maxSize)
+          print(head(fgseaOutUp))
+        fgseaOutDown<-fgsea(pathways = x[[1]],stats = statsDown, minSize=minSize, maxSize=maxSize)
+        print(head(fgseaOutDown))
+      } else{
+        fgseaOutUp<-fgsea(pathways = x[[1]],stats = statsUp, minSize=minSize, maxSize=maxSize, nperm=nperm)
+        fgseaOutDown<-fgsea(pathways = x[[1]],stats = statsDown, minSize=minSize, maxSize=maxSize, nperm=nperm)
+      }
+      print("fgseaOutUp")
+      print(fgseaOutUp)
+      print("fgseaOutDown")
+      print(fgseaOutDown)
+      fgseaOutUp<-fgseaOutUp[match(fgseaOutDown$pathway,fgseaOutUp$pathway),]
+      posGreater<-fgseaOutUp$ES>abs(fgseaOutDown$ES)
+      fgseaOut<-data.frame(ID=fgseaOutDown$pathway,Name=x[[2]][match(fgseaOutDown$pathway,x[[2]][,1]),2],fgseaOutDown[,-1],stringsAsFactors = F)
+      fgseaOut$ES<-(-fgseaOut$ES)
+      fgseaOut$NES<-(-fgseaOut$NES)
+      fgseaOut[posGreater,-c(1:2)]<-data.frame(fgseaOutUp[posGreater,-1],stringsAsFactors = F)
+      fgseaOut[order(fgseaOut$pval),]
+    })
+    names(fgseaRes)<-names(gs)
+    outlist=c(outlist,list(combined=fgseaRes))
+  }
+
+## fgsea calls not working
+# if("upnew" %in% directions){
+#   # stats=-log10(ifelse(diffexp>0,sigvals/2,1-sigvals/2))
+#   stats<-ifelse(diffexp>0,-log10(sigvals),log10(sigvals))
+#   names(stats)<-geneids
+#   stats=sort(stats)
+#   fgseaRes <- lapply(gs, function(x){
+#     if(is.null(nperm)) {
+#       fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize,scoreType="pos")
+#     } else{
+#       fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm,scoreType="pos")
+#     }
+#     print(head(fgseaOut))
+#     fgseaOut<-data.frame(ID=fgseaOut$pathway,Name=x[[2]][match(fgseaOut$pathway,x[[2]][,1]),2],fgseaOut[,-1],stringsAsFactors = F)
+#     fgseaOut[order(fgseaOut$pval),]
+#   })
+#   names(fgseaRes)<-names(gs)
+#   outlist=c(outlist,list(up=fgseaRes))
+# }
+#
+# if("downnew" %in% directions){
+#   # stats=-log10(ifelse(diffexp<0,sigvals/2,1-sigvals/2))
+#   stats<-ifelse(diffexp>0,-log10(sigvals),log10(sigvals))
+#   names(stats)<-geneids
+#   stats=sort(stats)
+#   fgseaRes <- lapply(gs, function(x){
+#     if(is.null(nperm)) {
+#       fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize,scoreType="neg")
+#     } else{
+#       fgseaOut<-fgsea(pathways = x[[1]],stats = stats, minSize=minSize, maxSize=maxSize, nperm=nperm,scoreType="neg")
+#     }
+#     fgseaOut<-data.frame(ID=fgseaOut$pathway,Name=x[[2]][match(fgseaOut$pathway,x[[2]][,1]),2],fgseaOut[,-1],stringsAsFactors = F)
+#     fgseaOut[order(fgseaOut$pval),]
+#   })
+#   print(head(fgseaOut))
+#   names(fgseaRes)<-names(gs)
+#   outlist=c(outlist,list(down=fgseaRes))
+# }
+
+
 
   if(excel){
     require(xlsx)
